98cb452a577af355fa628de0dd1ca5e8ab8df6a3
Kobi S <kobi.shasha@gmail.com>
2023-03-02 15:07:14 +0200

    Added Endpoints

--- src/main/java/com/jb/demo/DemoApplication.java
+++ src/main/java/com/jb/demo/DemoApplication.java
@@ -3,19 +3,27 @@ package com.jb.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/")
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping("hi")
-	public String hi(){
-		return "Hello Yaniv!";
-	}
+    @GetMapping
+    public String root() {
+        return "App is running!!!";
+    }
+
+    @GetMapping("hi")
+    public String hi(@RequestParam(defaultValue = "", required = false) String val) {
+        return (val.isBlank() ? "Hello There!" : String.format("Hello %s!", val));
+    }
 
 }
